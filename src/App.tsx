3ec6c92afcee989244c09bd1f7ea87cd3a5d6a3e
caokejian_i <caokejian_i@didiglobal.com>
2024-02-21 14:01:53 +0800

    feat: 简单交互

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,51 @@
-import { defineComponent } from 'vue';
+import { PropType, defineComponent, onMounted, ref } from 'vue';
+import axios from 'axios'
+
 export const App = defineComponent({
-  setup: () => () => (
-    <div></div>
-  )
+  setup: (props, context) => {
+    const socket = ref<WebSocket | null>(null)
+    const messages = ref<{ text: string, _id: string }[]>([])
+    const newMessage = ref<string>('')
+    const initInfo = async () => {
+      const res = await axios.get('http://localhost:8080/api/info')
+      messages.value = res.data
+    }
+    onMounted(() => {
+      const ws = new WebSocket('ws://localhost:8080');
+      initInfo()
+      ws.onopen = () => {
+        console.log('WebSocket 已连接');
+        socket.value = ws;
+      };
+
+      ws.onmessage = (event) => {
+        const message = JSON.parse(event.data);
+        messages.value.push(message);
+      };
+    });
+    const sendMessage = () => {
+      if (!socket.value || newMessage.value.trim() === '') return;
+
+      const message: any = {
+        text: newMessage.value.trim()
+      };
+
+      socket.value.send(JSON.stringify(message));
+      messages.value.push(message)
+      newMessage.value = '';
+    }
+
+    return () => (
+      <div>
+        <input type="text" v-model={newMessage.value} />
+        <button onClick={sendMessage}>submit</button>
+
+        <ul>
+          {messages.value.map(item => {
+            return <li key={item._id}>{item.text}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
 })
